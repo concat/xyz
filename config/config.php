d6770d31cd950af3b97b891280a7bd7d8311ffc1
Stephen Beck <beckstep@amazon.com>
2017-09-11 17:47:00 +0000

    Added Prod URL to config.php

--- config/config.php
+++ config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://54.82.241.53/xyz';
+$config['base_url'] = 'http://www.rstephenbeck.club/xyz';
 /*
 |--------------------------------------------------------------------------
 | Index File
